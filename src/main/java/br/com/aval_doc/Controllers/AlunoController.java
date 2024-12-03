6e4c9a1dde94004c60266d64b703559e2e0f7392
Natan Ferreira <78124517+Natanf0@users.noreply.github.com>
2024-12-03 00:44:17 -0300

    Update AlunoController.java

--- src/main/java/br/com/aval_doc/Controllers/AlunoController.java
+++ src/main/java/br/com/aval_doc/Controllers/AlunoController.java
@@ -3,6 +3,7 @@ package br.com.aval_doc.Controllers;
 import br.com.aval_doc.DTOs.AlunoDetailsDTO;
 import br.com.aval_doc.DTOs.AlunoInsertDTO;
 import br.com.aval_doc.Services.AlunoService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -21,13 +22,13 @@ public class AlunoController {
     public ResponseEntity<List<AlunoDetailsDTO>> listAllAlunos(@RequestParam(defaultValue ="0") int page,
                                                                @RequestParam(defaultValue = "5") int size) {
         return ResponseEntity.ok(alunoService.fetchAll(page, size));}
-    @Secured("COORDENACAO")
+
     @GetMapping("/{id}")
     public ResponseEntity<AlunoDetailsDTO> fetchAlunoById(@PathVariable Long id){
        return ResponseEntity.ok(alunoService.fetchById(id));
     }
     @PostMapping()
-    public ResponseEntity<AlunoDetailsDTO> insertAluno(@RequestBody AlunoInsertDTO alunoInsertDTO){
+    public ResponseEntity<AlunoDetailsDTO> insertAluno(@RequestBody @Valid AlunoInsertDTO alunoInsertDTO){
         return ResponseEntity.ok(alunoService.create(alunoInsertDTO));
     }
 
