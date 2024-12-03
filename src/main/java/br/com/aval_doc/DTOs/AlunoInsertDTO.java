48313afe7b13c050edc775a8902d2486cefb906f
Natan Ferreira <78124517+Natanf0@users.noreply.github.com>
2024-12-03 00:46:20 -0300

    Update AlunoInsertDTO.java

--- src/main/java/br/com/aval_doc/DTOs/AlunoInsertDTO.java
+++ src/main/java/br/com/aval_doc/DTOs/AlunoInsertDTO.java
@@ -1,10 +1,11 @@
 package br.com.aval_doc.DTOs;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
-public record AlunoInsertDTO(@Getter String DRE,
-                             @Getter String Nome,
-                             @Getter String Email_Inst,
-                             @Getter String Senha,
+public record AlunoInsertDTO(@NotBlank(message = "Informe o DRE") @Getter String DRE,
+                             @NotBlank(message = "Informe o Nome") @Getter String Nome,
+                             @NotBlank(message = "Informe o E-mail Institucional") @Getter String Email_Inst,
+                             @NotBlank(message = "Informe a senha") @Getter String Senha,
                              @Getter int fk_curso) {
 }
