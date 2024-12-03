e4e746ce79ab61b52b048131011c3c84ba003759
Natan Ferreira <78124517+Natanf0@users.noreply.github.com>
2024-12-03 00:47:11 -0300

    Update AvaliacaoUpdateDTO.java

--- src/main/java/br/com/aval_doc/DTOs/AvaliacaoUpdateDTO.java
+++ src/main/java/br/com/aval_doc/DTOs/AvaliacaoUpdateDTO.java
@@ -1,17 +1,22 @@
 package br.com.aval_doc.DTOs;
 
 import br.com.aval_doc.Entities.Avaliacao;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
 public record AvaliacaoUpdateDTO(
-        @Getter String descricao,
-        @Getter short didatica,
-        @Getter short atrasos,
-        @Getter short metodoAvaliacao,
-        @Getter short decoro
+        @NotBlank @Getter String descricao,
+        @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+        short didatica,
+        @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+        short atrasos,
+        @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+        short metodoAvaliacao,
+        @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+        short decoro
 ) {
 
 }
