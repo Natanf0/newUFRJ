bcb0258c9a42d7944cf4ea556c69d2660e5b2f8c
Natan Ferreira <78124517+Natanf0@users.noreply.github.com>
2024-12-03 00:46:49 -0300

    Update AvaliacaoCreateDTO.java

--- src/main/java/br/com/aval_doc/DTOs/AvaliacaoCreateDTO.java
+++ src/main/java/br/com/aval_doc/DTOs/AvaliacaoCreateDTO.java
@@ -1,20 +1,27 @@
 package br.com.aval_doc.DTOs;
 
 import br.com.aval_doc.Entities.Avaliacao;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.time.LocalDate;
 
-public record AvaliacaoCreateDTO(@Getter String descricao,
-                                 @Getter short didatica,
-                                 @Getter short atrasos,
-                                 @Getter short metodoAvaliacao,
-                                 @Getter short decoro,
-                                 @Getter Long fk_aluno,
-                                 @Getter int fk_professor,
-                                 @Getter String fk_disciplina,
-                                 @Getter LocalDate dataAvaliacao) {
+public record AvaliacaoCreateDTO(@NotBlank @Getter String descricao,
+                                 @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+                                 short didatica,
+                                 @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+                                 short atrasos,
+                                 @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+                                 short metodoAvaliacao,
+                                 @NotEmpty @Min(value = 0, message = "Informe um valor entre 0 e 10") @Max(value = 10, message = "Informe um valor entre 0 e 10") @Getter
+                                 short decoro,
+                                 @NotEmpty @Getter
+                                 Long fk_aluno,
+                                 @NotEmpty @Getter
+                                 int fk_professor,
+                                 @NotBlank @Getter String fk_disciplina,
 
+                                 @Getter LocalDate dataAvaliacao) {
 
     public static AvaliacaoCreateDTO convertAvaliacaoToAvaliacaoCreateDTO(Avaliacao avaliacao) {
         return new AvaliacaoCreateDTO(avaliacao.getDescricao(),
